b1db7f9d7de1c50c74919f2efaba1881b31fdff6
tinybright <774994940@qq.com>
2017-04-25 09:43:05 +0800

    init

--- library/src/main/java/com/rafakob/drawme/DrawMeTextView.java
+++ library/src/main/java/com/rafakob/drawme/DrawMeTextView.java
@@ -8,27 +8,44 @@ import com.rafakob.drawme.delegate.DrawMe;
 import com.rafakob.drawme.delegate.DrawMeShapeText;
 
 public class DrawMeTextView extends TextView {
-    private final DrawMe drawMe;
+    private DrawMe drawMe;
 
     public DrawMeTextView(Context context) {
         super(context);
-        drawMe = new DrawMeShapeText(context, this, null);
+        if(isInEditMode()){
+
+        }else{
+            drawMe = new DrawMeShapeText(context, this, null);
+        }
     }
 
     public DrawMeTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        drawMe = new DrawMeShapeText(context, this, attrs);
+        if(isInEditMode()){
+
+        }else{
+            drawMe = new DrawMeShapeText(context, this, attrs);
+        }
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        int[] size = drawMe.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        super.onMeasure(size[0], size[1]);
+        if(isInEditMode()){
+            super.onMeasure(widthMeasureSpec,heightMeasureSpec);
+        }else{
+            int[] size = drawMe.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            super.onMeasure(size[0], size[1]);
+        }
+
     }
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
-        drawMe.onLayout(changed, left, top, right, bottom);
+        if(isInEditMode()){
+
+        }else{
+            drawMe.onLayout(changed, left, top, right, bottom);
+        }
     }
 }
